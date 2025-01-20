d2737ec986c2371dc4d903a89e3830e622c7743f
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-01-20 14:52:54 +0100

    Ajout table.js

---
+++ boucles.js
@@ -0,0 +1,17 @@
+console.log("Boucle for");
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+console.log("Boucle while");
+let i = 0;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
