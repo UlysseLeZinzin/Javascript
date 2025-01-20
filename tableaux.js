224b30efd8942f696072105e5cbda0f47d72838a
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-01-20 15:54:51 +0100

    Create tableaux.js

---
+++ tableaux.js
@@ -0,0 +1,10 @@
+// Variable d etype string, initialisée avec une chaine de craractères
+let etudiant = "Jean-Marie-LePaing";
+
+//Varaiable de type tableau, initialisée avec une liste de chaines de caractères
+let etudiants = ["Jean-Marie-Marie", "Jean-Michel-Michel", "Jean-Luc-Luc", "Jean-Pierre-Pierre"];
+
+console.log("etudiant", etudiant);
+console.log("etudiants", etudiants);
+
+console.log("taille du tableau", etudiant.length);
