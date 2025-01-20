7a08d26cbb51d4a0f590cd1b0588404da25bc5c5
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-01-20 15:03:41 +0100

    Update tables.js

--- table.js
+++ table.js
@@ -1,10 +1,10 @@
 // boucle for pour afficher la table de multiplication de 7
 for (let i = 0; i <= 10;){
-    console.log(7 * i++);
+    console.log("7 * " + i + " = " + 7 * i++);
 }
 
 // boucle while pour afficher la table de multiplication de 5
 let i = 0;
 while (i <= 10){
-    console.log(5 * i++);
+    console.log("5 * " + i + " = " + 5 * i++);
 }
