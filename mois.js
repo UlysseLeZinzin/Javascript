6cc30d6a4d1d95a66eb30f1addd503cceb3b322b
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-02-03 10:04:25 +0100

    Create mois.js

---
+++ mois.js
@@ -0,0 +1,10 @@
+function mois(i) {
+    //retourner la chaine de caractères correspondant au mois nupérique demandé
+    //en utilisant un tableau
+
+    //Tableau des mois
+    let mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet",
+        "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    //Retourner le mois demandé
+    return mois[i - 1];
+}
