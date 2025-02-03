5008f773aaf76ba2e2e2bef9620eb241527ed42f
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-02-03 10:21:09 +0100

    Create compte.js

---
+++ compte.js
@@ -0,0 +1,28 @@
+// Créer un objet compte (compte bancaire) avec les propriétés suivantes :
+// client
+// numéro
+// solde
+// avec les méthodes suivantes :
+// afficher: affiche toutes les propriétés du compte
+// crediter: ajoute un montant au solde
+// debiter: enleve un montant au solde
+
+let compte = {
+    client: "client",
+    numero: "numero",
+    solde: 0,
+    afficher: function() {
+        console.log(this.client, this.numero, this.solde);
+    },
+    crediter: function(montant) {
+        this.solde += montant;
+        console.log(this.solde);
+    },
+    debiter: function(montant) {
+        this.solde -= montant;
+        console.log(this.solde);
+    }
+}
+compte.afficher();
+compte.crediter(100);
+compte.debiter(50);
